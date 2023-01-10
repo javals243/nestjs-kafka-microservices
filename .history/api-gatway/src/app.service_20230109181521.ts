12ede8a9023a7259c3a6263b7975d9db8e2fb60a
valere chihisa <vchihisa@gmail.com>
2023-01-10 22:07:09 +0200

    Add a Create order services and controller

--- .history/api-gatway/src/app.service_20230109181521.ts
+++ .history/api-gatway/src/app.service_20230109181521.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { CreateOrderRequest } from './create-order-request.dto';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
-  CreateOrder({ userId, price }: CreateOrderRequest) {}
 }
